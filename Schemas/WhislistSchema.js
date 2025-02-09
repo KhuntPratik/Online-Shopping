f73f9f14bbcc89eb43e54e017b1a654b0760a688
KhuntPratik <140686802+KhuntPratik@users.noreply.github.com>
2025-02-09 09:19:32 +0530

    remove schemas

--- Schemas/WhislistSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = mongoose.Schema({
-  WhislistId: Number,
-  ProductID: Number,
-  UserId:Number
-})
-
-module.exports = mongoose.model('whislists', schema)
